ea487cdb682cd61117596b748c7a78fa54f2fc57
Peter <pjb45@duke.edu>
2024-12-07 23:39:10 -0500

    silly goose forgot a comma

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def train():
         l_temp=1.0,
         learning_rate_d=0.0001,
         learning_rate_g=0.0002,
-        learning_rate_p=0.0002
+        learning_rate_p=0.0002,
         lr_warmup_epochs=10
     )
 
